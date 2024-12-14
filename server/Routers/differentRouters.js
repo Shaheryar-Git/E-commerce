568b93ba9c28e14bace342b822fd419a6c7143e9
shaheryar <shaheryarsherry114@gmail.com>
2024-12-14 21:52:17 +0500

    deploy commit

--- server/Routers/differentRouters.js
+++
@@ -1,9 +0,0 @@
-import  express from "express";
-import formController from "../Controllers/formController.js";
-const Routers = express.Router()
-
-
-Routers.post("/signIn",formController)
-
-
-export default Routers
